ec6d6fa90086bf5aeb7f95061f603b736f46036e
Matheus Willian <matheuswillian91@hotmail.com>
2021-07-03 11:21:43 -0300

    context

---
+++ ProjetoEngSoftII/Data/ProjectContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProjetoEngSoftII.Models;
+
+namespace ProjetoEngSoftII.Data
+{
+    public class ProjectContext : DbContext
+    {
+        protected ProjectContext()
+        {
+        }
+
+        public ProjectContext(DbContextOptions<ProjectContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Pessoa> Pessoa { get; set; }
+    }
+}
--- ProjetoEngSoftII/Startup.cs
+++ ProjetoEngSoftII/Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ProjetoEngSoftII.Data;
 
 namespace ProjetoEngSoftII
 {
@@ -24,6 +26,9 @@ namespace ProjetoEngSoftII
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            var connectionString = Configuration.GetConnectionString("ProjectConnectionString");
+            services.AddDbContext<ProjectContext>(options => options.UseNpgsql(connectionString));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
