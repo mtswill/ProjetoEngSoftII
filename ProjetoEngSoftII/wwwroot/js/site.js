129e94ff35c6a388fdf6c52d2322b20e2a88235e
Matheus Willian <matheuswillian91@hotmail.com>
2021-07-09 10:22:47 -0300

    value

--- ProjetoEngSoftII/wwwroot/js/site.js
+++ ProjetoEngSoftII/wwwroot/js/site.js
@@ -1,9 +1,11 @@
 ﻿var testeCont = document.getElementById("container-info-paciente-inserir-vacinado");
+var txtstart = document.getElementById("txtstart");
 
 $('#teste-button').on("click", function () {
 
     if (testeCont.style.display === "none") {
         testeCont.style.display = "block";
+        txtstart.value = "teste";
     } else {
         testeCont.style.display = "none";
     }
