6aa976c275deb714b0c2a38b42054f5fb6d7f173
Matheus Willian <matheuswillian91@hotmail.com>
2021-07-08 21:48:22 -0300

    seedingservices

---
+++ ProjetoEngSoftII/Data/SeedingService.cs
@@ -0,0 +1,80 @@
+﻿using ProjetoEngSoftII.Models.Vacinas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoEngSoftII.Data
+{
+    public class SeedingService
+    {
+        private readonly ProjectContext _context;
+
+        public SeedingService(ProjectContext context)
+        {
+            _context = context;
+        }
+
+        internal void Seed()
+        {
+            #region Verificações
+            
+            if (_context.MarcaVacinaCovid.Any())
+            {
+                return;
+            }
+
+            #endregion Verificações
+
+            #region Preenche marcas vacina
+
+            var pfizer = new MarcaVacinaCovid
+            {
+                Marca = "Pfizer-BioNTech",
+                Tipo = "mRNA"
+            };
+
+            var moderna = new MarcaVacinaCovid
+            {
+                Marca = "Moderna",
+                Tipo = "mRNA"
+            };
+
+            var sputnik = new MarcaVacinaCovid
+            {
+                Marca = "Sputnik V",
+                Tipo = "Adenovírus"
+            };
+
+            var oxford = new MarcaVacinaCovid
+            {
+                Marca = "Oxford-AstraZeneca",
+                Tipo = "Adenovírus"
+            };
+
+            var covishield = new MarcaVacinaCovid
+            {
+                Marca = "Covidshield",
+                Tipo = "Adenovírus"
+            };
+
+            var janssen = new MarcaVacinaCovid
+            {
+                Marca = "Janssen",
+                Tipo = "Adenovírus"
+            };
+
+            var cansino = new MarcaVacinaCovid
+            {
+                Marca = "Cansino",
+                Tipo = "Adenovírus"
+            };
+
+            _context.MarcaVacinaCovid.AddRange(pfizer, moderna, sputnik, oxford, covishield, janssen, cansino);
+
+            #endregion Preenche marcas vacina
+
+            _context.SaveChanges();
+        }
+    }
+}
--- ProjetoEngSoftII/Startup.cs
+++ ProjetoEngSoftII/Startup.cs
@@ -34,14 +34,16 @@ namespace ProjetoEngSoftII
 
             //DI
             services.AddScoped<PacienteRepository>();
+            services.AddScoped<SeedingService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, SeedingService seedingService)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                seedingService.Seed();
             }
             else
             {
