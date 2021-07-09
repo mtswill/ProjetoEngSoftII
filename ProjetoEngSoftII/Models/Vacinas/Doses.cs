fe73cf812007e02dc2ef388feb101f4729f8934b
Matheus Willian <matheuswillian91@hotmail.com>
2021-07-08 23:19:00 -0300

    lista de doses

---
+++ ProjetoEngSoftII/Models/Vacinas/Doses.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoEngSoftII.Models.Vacinas
+{
+    public static class Doses
+    {
+        public const string PrimeiraDose = "1ª dose";
+        public const string SegundaDose = "2ª dose";
+
+        public static List<string> GetDoses()
+        {
+            return new List<string>
+            {
+                PrimeiraDose,
+                SegundaDose
+            };
+        }
+    }
+}
