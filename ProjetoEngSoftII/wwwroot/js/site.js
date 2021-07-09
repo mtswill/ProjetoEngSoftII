4e322130ffdb0a180fb63a20e5bcc6aa8eb17dc3
Matheus Willian <matheuswillian91@hotmail.com>
2021-07-09 10:11:51 -0300

    display js

--- ProjetoEngSoftII/wwwroot/js/site.js
+++ ProjetoEngSoftII/wwwroot/js/site.js
@@ -1,13 +1,10 @@
-﻿var testeCont = document.getElementById("teste-container");
+﻿var testeCont = document.getElementById("container-info-paciente-inserir-vacinado");
 
 $('#teste-button').on("click", function () {
-    //testeCont.style.backgroundColor = "green";
 
-    if (testeCont.style.backgroundColor === "green") {
-        window.alert("mudou");
-        testeCont.style.backgroundColor = "red";
-    }
-    else {
-        testeCont.style.backgroundColor = "red";
+    if (testeCont.style.display === "none") {
+        testeCont.style.display = "block";
+    } else {
+        testeCont.style.display = "none";
     }
 });
