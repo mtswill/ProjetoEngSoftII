37f38f3683005d1116018056a87ad17958fb7e4a
Matheus Willian <matheuswillian91@hotmail.com>
2021-07-15 21:44:46 -0300

    modelo carteira vacinacao covid

--- ProjetoEngSoftII/Helpers/PdfCreator.cs
+++ ProjetoEngSoftII/Helpers/PdfCreator.cs
@@ -1,4 +1,5 @@
-﻿using PdfSharp.Drawing;
+﻿using PdfSharp;
+using PdfSharp.Drawing;
 using PdfSharp.Drawing.Layout;
 using ProjetoEngSoftII.Models.Pdf;
 using System;
@@ -9,19 +10,22 @@ namespace ProjetoEngSoftII.Helpers
 {
     public class PdfCreator
     {
+        private string pathImagemModelo = Directory.GetCurrentDirectory() + @"\Files\modelo_carteira_vacinacao_covid.png";
+
         public void CreatePdf(VacinadoPdfModel vacinado)
         {
             using (var doc = new PdfSharp.Pdf.PdfDocument())
             {
                 var page = doc.AddPage();
+                page.Orientation = PageOrientation.Landscape;
                 var graphics = XGraphics.FromPdfPage(page);
                 var textFormatter = new XTextFormatter(graphics);
-                var font = new XFont("Arial", 14, XFontStyle.BoldItalic);
+                var font = new XFont("Verdana", 13, XFontStyle.Regular);
+
+                graphics.DrawImage(XImage.FromFile(pathImagemModelo), 250, 300);
 
                 textFormatter.Alignment = XParagraphAlignment.Center;
-                textFormatter.DrawString("Matheus Willian Polato", font, XBrushes.Black, new XRect(0, 50, page.Width, page.Height));
-
-                graphics.DrawImage(XImage.FromFile(@"D:\foto.jpg"), 250, 300);
+                textFormatter.DrawString(vacinado.Nome, font, XBrushes.Black, new XRect(0, 50, page.Width, page.Height));
 
                 var nomeArquivo = $"{vacinado.Cpf.RemovePontoEHifem()}.pdf";
                 var path = Directory.GetCurrentDirectory() + @"\Files\" + nomeArquivo;
