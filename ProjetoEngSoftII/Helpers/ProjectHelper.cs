a80130566a1068b095d46abc0d30c25a74f765dd
Matheus Willian <matheuswillian91@hotmail.com>
2021-07-19 23:04:12 -0300

    a

--- ProjetoEngSoftII/Helpers/ProjectHelper.cs
+++ ProjetoEngSoftII/Helpers/ProjectHelper.cs
@@ -9,5 +9,25 @@ namespace ProjetoEngSoftII.Helpers
     {
         public static string RemovePontoEHifem(this string obj)
             => obj.Replace(".", string.Empty).Replace("-", string.Empty);
+
+        public static string InserePontoEHifemCpf(this string obj)
+        {
+            var primeiraParte = obj.Substring(0, 3);
+            var segundaParte = obj.Substring(4, 3);
+            var terceiraParte = obj.Substring(7, 3);
+            var quartaParte = obj.Substring(9);
+
+            return $"{primeiraParte}.{segundaParte}.{terceiraParte}-{quartaParte}";
+        }
+        
+        public static string InserePontoEHifemRg(this string obj)
+        {
+            var primeiraParte = obj.Substring(0, 2);
+            var segundaParte = obj.Substring(2, 3);
+            var terceiraParte = obj.Substring(5, 3);
+            var quartaParte = obj.Substring(8);
+
+            return $"{primeiraParte}.{segundaParte}.{terceiraParte}-{quartaParte}";
+        }
     }
 }
